01faf2e736bfdf0e1edaf524938499b07ac793b0
Carla Barazarte <cbarazarte7@gmail.com>
2023-02-27 14:49:29 -0500

    add new book functionality

--- server/routes/books.js
+++ server/routes/books.js
@@ -29,6 +29,7 @@ router.get('/add', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
+    res.render('books/details',{title:'Add Book', books: {}})
 
 });
 
@@ -38,7 +39,22 @@ router.post('/add', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
-
+    let newBook = book({
+      "Title": req.body.title,
+      "Description": req.body.description,
+      "Price": req.body.price,
+      "Author": req.body.author,
+      "Genre": req.body.genre
+  });
+  book.create(newBook, (err, book) => {
+      if (err) {
+          console.log(err);
+          res.end(err);
+      }
+      else {
+          res.redirect('/books');
+      }
+  });
 });
 
 // GET the Book Details page in order to edit an existing Book
