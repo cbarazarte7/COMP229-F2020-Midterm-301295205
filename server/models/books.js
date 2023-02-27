251ea2b5802048ac56e1752987038da187adfec2
Carla Barazarte <cbarazarte7@gmail.com>
2023-02-27 14:42:31 -0500

    add default data to books database

--- server/models/books.js
+++ server/models/books.js
@@ -12,4 +12,53 @@ let Book = mongoose.Schema({
   collection: "books"
 });
 
-module.exports = mongoose.model('Book', Book);
+const collection = mongoose.model('Book', Book);
+
+const defaultBooks = [{
+ 
+  // Inserting default values
+  Title: "The water",
+  Description: "The water is about a shark who terrorized...",
+  Price: 20,
+  Author: "Mike Tae",
+  Genre: "Horror"
+},
+{
+
+  // Inserting default values
+  Title: "Friends",
+  Description: "Friends is about 3 friends who struggled...",
+  Price: 23.5,
+  Author: "Laura Hardy",
+  Genre: "Comedy"
+},
+{
+
+  // Inserting default values
+  Title: "Before Sunset",
+  Description: "Before Sunset is about a marriage couple...",
+  Price: 23.5,
+  Author: "Ben Queen",
+  Genre: "Romance"
+},{
+
+  // Inserting default values
+  Title: "My Life",
+  Description: "My Life is about my childhood...",
+  Price: 15.3,
+  Author: "Max Paint",
+  Genre: "Biography",
+}]
+collection.bulkWrite(
+  defaultBooks.map((defaultBook) => 
+    ({
+      updateOne: {
+        filter: { Title : defaultBook.Title },
+        update: { $set: defaultBook },
+        upsert: true
+      }
+    })
+  )
+)
+
+module.exports = collection;
